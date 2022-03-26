090a9fdd9e1a6f2bb8a59ae2f2cbff512c9a6512
David Ogar <owogogahhero@outlook.com>
2022-03-26 08:10:57 +0100

    added dialog component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+import { ShareComponent } from './share/share.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'angular-forms-project';
+
+  constructor(private dialog : MatDialog){}
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '40%',
+      height: '40%'
+      //will parse in the data
+    });
+  }
+
+  openShare(){
+    this.dialog.open(ShareComponent, {
+      width: '40%',
+      height: '40%'
+    })
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+import { ShareComponent } from './share/share.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent,
+    ShareComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
